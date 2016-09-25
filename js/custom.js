feac0bcd655df90a100c307a1f480cadd9d921f8
Xinyi Cen <cenxinyiapp@gmail.com>
2016-09-25 18:11:40 -0500

    modified Portfolio section in index page

--- js/custom.js
+++ js/custom.js
@@ -62,7 +62,25 @@
 	});
 	
 	//owl carousel
-	$('#owl-works').owlCarousel({
+	$('#owl-works-architecture').owlCarousel({
+            items : 4,
+            itemsDesktop : [1199,5],
+            itemsDesktopSmall : [980,5],
+            itemsTablet: [768,5],
+            itemsTabletSmall: [550,2],
+            itemsMobile : [480,2],
+        });
+
+	$('#owl-works-website').owlCarousel({
+            items : 4,
+            itemsDesktop : [1199,5],
+            itemsDesktopSmall : [980,5],
+            itemsTablet: [768,5],
+            itemsTabletSmall: [550,2],
+            itemsMobile : [480,2],
+        });
+
+	$('#owl-works-product').owlCarousel({
             items : 4,
             itemsDesktop : [1199,5],
             itemsDesktopSmall : [980,5],
